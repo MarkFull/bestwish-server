5a395cc861d5ea7a97a3e2a7ae6f3ea3bc783bd9
Mark Fu Shengwen <markfsw@gmail.com>
2013-09-02 11:32:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bestwish::Application.config.secret_key_base = '0af982d542bb48141ed0b2722108bc291bb4aaf57d3e8979a6521c23c3468ebe612d98697aeda2f7c19abe9ef0ec87eaf88505c9fb9d38e4bc218d3f5834d93b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bestwish::Application.config.session_store :cookie_store, key: '_bestwish_session'
